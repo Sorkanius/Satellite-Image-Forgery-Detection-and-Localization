c7e2992ee96e09c62cad855c67912026e0456b59
ignacio <ignacio@peletier.com>
2019-05-14 10:17:57 +0200

    Changes in optimized detection

--- sliding_window_with_attacks_optimized.py
+++ sliding_window_with_attacks_optimized.py
@@ -13,7 +13,7 @@ def crop(im, height=64, width=64, stride=1):
     img_height = im.shape[0]
     img_width = im.shape[1]
     ssim = []
-    for i in tqdm.tqdm(range(0, (img_height - height)//stride + 1)):
+    for i in range(0, (img_height - height)//stride + 1):
         img = np.array([])
         for j in range(0, (img_width - width)//stride + 1):
                 img = np.append(img, im[i*stride: i*stride + height, j*stride: j*stride + width])
@@ -39,7 +39,7 @@ def mse(image_A, image_B):
     return err
 
 
-images_folder = 'data/test_data/'
+images_folder = 'data/train_data/'
 images = os.listdir(images_folder)
 threshold = 0.9777527527527528
 
@@ -49,84 +49,98 @@ from adversarial_autoencoder import *
 aae = AdversarialAutoencoder()
 aae.autoencoder.load_weights('trainings/no_gan/models/low_ae_autoencoder.h5')
 
-image = 'forged_forged_forged_115_64_104_394_3_128_227_260_9_128_77_5_15.png'
-im_read = np.array(Image.open(os.path.join(images_folder, image)), dtype='uint8')
-ssim, rows, cols = crop(im_read, stride=8)
-
-images = os.listdir('slides')
 wind = window(640, 640, stride=8)
 plt.imshow(wind)
 plt.savefig('window.png')
 plt.close()
-prediction = np.zeros([640, 640])
-ones = np.ones([64, 64])
 
-ssim = np.asarray(ssim)
-ssim = ssim.reshape(rows, cols)
+attack_prop = range(0, 110, 10)
 
+mse_of_attack_ones = {}
+mse_of_attack_zeros = {}
+mse_of_attack_mix = {}
+mse_of_one_node = []
 
-# Let's start with attacks
+all_zeros = np.zeros([640, 640])
 
-attack_prop = range(10, 110, 10)
+for i in attack_prop:
+    mse_of_attack_ones[i] = []
+    mse_of_attack_zeros[i] = []
+    mse_of_attack_mix[i] = []
 
-mse_of_attack_ones = []
-mse_of_attack_zeros = []
-mse_of_attack_mix = []
-
-for i in tqdm.tqdm(range(0, (640 - 64)//8 + 1)):
-    for j in range(0, (640 - 64)//8 + 1):
-        if ssim[i, j] <= threshold:
-            prediction[i*8: i*8 + 64, j*8: j*8 + 64] = prediction[i*8: i*8 + 64, j*8: j*8 + 64] + ones
-
-for k in attack_prop:
-    attacks = np.random.binomial(1, k/100, [rows, cols])
-    prediction_ones = np.zeros([640, 640])
-    prediction_zeros = np.zeros([640, 640])
-    prediction_mix = np.zeros([640, 640])
-    for i in tqdm.tqdm(range(0, (640 - 64)//8 + 1)):
+for image in tqdm.tqdm(images):
+    im_read = np.array(Image.open(os.path.join(images_folder, image)), dtype='uint8')
+    ssim, rows, cols = crop(im_read, stride=8)
+    prediction = np.zeros([640, 640])
+    ones = np.ones([64, 64])
+    ssim = np.asarray(ssim)
+    ssim = ssim.reshape(rows, cols)
+    new_dir = 'postprocessing_with_attacks/{}/'.format(image)
+    os.makedirs(new_dir, exist_ok=True)
+    for i in range(0, (640 - 64)//8 + 1):
         for j in range(0, (640 - 64)//8 + 1):
-            if attacks[i, j] == 0:
-                if ssim[i, j] <= threshold:
+            if ssim[i, j] <= threshold:
+                prediction[i*8: i*8 + 64, j*8: j*8 + 64] = prediction[i*8: i*8 + 64, j*8: j*8 + 64] + ones
+    mse_of_one_node.append(mse(all_zeros, prediction/wind))
+    for k in attack_prop:
+        attacks = np.random.binomial(1, k/100, [rows, cols])
+        prediction_ones = np.zeros([640, 640])
+        prediction_zeros = np.zeros([640, 640])
+        prediction_mix = np.zeros([640, 640])
+        for i in range(0, (640 - 64)//8 + 1):
+            for j in range(0, (640 - 64)//8 + 1):
+                if attacks[i, j] == 0:
+                    if ssim[i, j] <= threshold:
+                        prediction_ones[i*8: i*8 + 64, j*8: j*8 + 64] = \
+                            prediction_ones[i*8: i*8 + 64, j*8: j*8 + 64] + ones
+                        prediction_zeros[i*8: i*8 + 64, j*8: j*8 + 64] = \
+                            prediction_zeros[i*8: i*8 + 64, j*8: j*8 + 64] + ones
+                        prediction_mix[i*8: i*8 + 64, j*8: j*8 + 64] = \
+                            prediction_mix[i*8: i*8 + 64, j*8: j*8 + 64] + ones
+                else:
                     prediction_ones[i*8: i*8 + 64, j*8: j*8 + 64] = \
                         prediction_ones[i*8: i*8 + 64, j*8: j*8 + 64] + ones
                     prediction_zeros[i*8: i*8 + 64, j*8: j*8 + 64] = \
-                        prediction_zeros[i*8: i*8 + 64, j*8: j*8 + 64] + ones
+                        prediction_zeros[i*8: i*8 + 64, j*8: j*8 + 64]
                     prediction_mix[i*8: i*8 + 64, j*8: j*8 + 64] = \
-                        prediction_mix[i*8: i*8 + 64, j*8: j*8 + 64] + ones
-            else:
-                prediction_ones[i*8: i*8 + 64, j*8: j*8 + 64] = \
-                    prediction_ones[i*8: i*8 + 64, j*8: j*8 + 64] + ones
-                prediction_zeros[i*8: i*8 + 64, j*8: j*8 + 64] = \
-                    prediction_zeros[i*8: i*8 + 64, j*8: j*8 + 64]
-                prediction_mix[i*8: i*8 + 64, j*8: j*8 + 64] = \
-                    prediction_mix[i*8: i*8 + 64, j*8: j*8 + 64] + np.random.binomial(1, 0.5, 1)[0]*ones
+                        prediction_mix[i*8: i*8 + 64, j*8: j*8 + 64] + np.random.binomial(1, 0.5, 1)[0]*ones
+        mse_of_attack_ones[k].append(mse(all_zeros, prediction_ones/wind))
+        mse_of_attack_zeros[k].append(mse(all_zeros, prediction_zeros/wind))
+        mse_of_attack_mix[k].append(mse(all_zeros, prediction_mix/wind))
+        fig, axs = plt.subplots(1, 5)
+        axs[0].imshow(im_read)
+        axs[1].imshow(prediction/wind)
+        axs[2].imshow(prediction_ones/wind, vmin=0.0, vmax=1.0)
+        axs[3].imshow(prediction_zeros/wind)
+        axs[4].imshow(prediction_mix/wind)
+        axs[0].axis('off')
+        axs[1].axis('off')
+        axs[2].axis('off')
+        axs[3].axis('off')
+        axs[4].axis('off')
+        fig.savefig(new_dir + 'attack_{}.png'.format(k))
+        plt.close()
 
-    mse_of_attack_ones.append(mse(prediction, prediction_ones))
-    mse_of_attack_zeros.append(mse(prediction, prediction_zeros))
-    mse_of_attack_mix.append(mse(prediction, prediction_mix))
-    fig, axs = plt.subplots(1, 5)
-    axs[0].imshow(im_read)
-    axs[1].imshow(prediction/wind)
-    axs[2].imshow(prediction_ones/wind, vmin=0.0, vmax=1.0)
-    axs[3].imshow(prediction_zeros/wind)
-    axs[4].imshow(prediction_mix/wind)
-    axs[0].axis('off')
-    axs[1].axis('off')
-    axs[2].axis('off')
-    axs[3].axis('off')
-    axs[4].axis('off')
-    fig.savefig('postprocessing_with_attacks/attack_{}_pred_{}.png'.format(k, image))
-    plt.close()
+mean_ones = []
+mean_zeros = []
+mean_mix = []
+for i in sorted(mse_of_attack_ones.keys()):
+    mean_ones.append(np.mean(mse_of_attack_ones[i]))
+    mean_zeros.append(np.mean(mse_of_attack_zeros[i]))
+    mean_mix.append(np.mean(mse_of_attack_mix[i]))
+
 
 plt.figure()
 plt.xlabel('% of attackers')
-plt.ylabel('MSE respect to one node')
-plt.plot(list(attack_prop), mse_of_attack_ones, c='C0', label='Always One attack')
-plt.plot(list(attack_prop), mse_of_attack_zeros, c='C1', label='Always Zero attack')
-plt.plot(list(attack_prop), mse_of_attack_mix, c='C2', label='Random attack')
+plt.ylabel('MSE')
+plt.semilogy(list(attack_prop), mean_ones, c='C0', label='Always One attack')
+plt.semilogy(list(attack_prop), mean_zeros, c='C1', label='Always Zero attack')
+plt.semilogy(list(attack_prop), mean_mix, c='C2', label='Random attack')
+plt.semilogy(list(attack_prop), np.repeat(np.mean(mse_of_one_node), len(list(attack_prop))), c='C4',
+             label='Centralized Node')
 plt.legend()
 plt.grid()
-plt.savefig('postprocessing_with_attacks/mse_{}.png'.format(image))
+plt.savefig('postprocessing_with_attacks/mse.png')
 plt.close()
 
 
